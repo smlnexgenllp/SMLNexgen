47a9f2c0e03880a1b1a64a2963a2a59f7ffed47f
Udhai2039 <udhai2039@gmail.com>
2025-03-17 10:45:55 +0530

    Update Navbar.js

--- src/app/Careers/components/Navbar.js
+++ src/app/Careers/components/Navbar.js
@@ -6,7 +6,8 @@ import { useRouter } from "next/navigation";
 import { FaUser, FaCog, FaSignOutAlt, FaBars, FaTimes } from "react-icons/fa";
 import styles from "../styles/nav.module.css";
 
-const API_BASE_URL = "https://sml-backend-qgp6.onrender.com"; // Adjust the URL as needed
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+ // Adjust the URL as needed
 
 const Navbar = () => {
   const [user, setUser] = useState(null);
@@ -100,10 +101,10 @@ const Navbar = () => {
               </span>
               <Image
                 src={
-                  user.profilePic
-                    ? `${API_BASE_URL}${user.profilePic}`
-                    : "/placeholder-avatar.png"
-                }
+    user.profilePic
+      ? `${API_BASE_URL}${user.profilePic}`
+      : "/placeholder-avatar.png"
+  }
                 alt="Profile"
                 width={30}
                 height={30}
