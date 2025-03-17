acd45c5bc862ed7404823d369e2282aa47489b8e
Udhai2039 <udhai2039@gmail.com>
2025-03-17 15:12:02 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["unsplash.it", "https://www.smlnexgenllp.com/"],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sml-backend-qgp6.onrender.com", // Allow images from your backend
+      },
+    ],
+  },
+};
+
+export default nextConfig; // Use export default instead of module.exports
