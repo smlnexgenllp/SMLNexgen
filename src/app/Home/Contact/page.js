6cb7bf16dda0c24391cb80d80cc7b1ab8ce558a3
Udhai2039 <udhai2039@gmail.com>
2025-03-18 21:11:32 +0530

    Update page.js

--- src/app/Home/Contact/page.js
+++ src/app/Home/Contact/page.js
@@ -59,7 +59,7 @@ const ContactForm = () => {
       };
 
       try {
-        const response = await fetch('https://sml-backend-qgp6.onrender.com/api/contact', {
+        const response = await fetch('http://3.110.45.103:5000/api/contact', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
