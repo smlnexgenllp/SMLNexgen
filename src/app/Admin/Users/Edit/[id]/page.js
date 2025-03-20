4b3ec93657b61f0e1383af3f4dd3ac7e8e05a2c0
Udhai2039 <udhai2039@gmail.com>
2025-03-20 10:25:37 +0530

    Update page.js

--- src/app/Admin/Users/Edit/[id]/page.js
+++ src/app/Admin/Users/Edit/[id]/page.js
@@ -26,7 +26,7 @@ export default function EditUserPage({ params }) {
   const [showCropper, setShowCropper] = useState(false);
 
   // API base URL
-  const API_BASE_URL = 'http://192.168.0.197:5000';
+  const API_BASE_URL = 'https://sml-backend-qgp6.onrender.com';
 
   useEffect(() => {
     const authToken = Cookies.get("authToken");
